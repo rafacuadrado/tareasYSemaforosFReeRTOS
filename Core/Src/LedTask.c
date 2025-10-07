49578c4bc5820afeef93d90c10c2cc4c8dccee91
rafa_cuadrado <90984220+rafacuadrado@users.noreply.github.com>
2025-10-07 17:17:33 +0200

    commit ej2 terminado

--- Core/Src/LedTask.c
+++ Core/Src/LedTask.c
@@ -11,6 +11,7 @@ int led2=2;
 void createLedTask(){
 	xTaskCreate(ledToggleTask, "ledToggleTask 1", 128, &led1, 1, NULL);
 	xTaskCreate(ledToggleTask, "ledToggleTask 2", 128, &led2, 2, NULL);
+	//xTaskCreate(ledToggleTask, "ledToggleTask 2", 128, &led2, 1, NULL);
 
 }
 /*
@@ -20,6 +21,18 @@ void ledToggleTaskej1(void * pargs){
 		vTaskDelay(300);
 	}
 }*/
+void ledToggleTaskej1b(void * pargs){
+	while(1){
+	int led=*(int*)pargs;
+	if(led==1){
+		LED_Toggle(1);
+			vTaskDelay(300);
+	}else{
+		LED_Toggle(2);
+		vTaskDelay(300);
+	}
+	}
+}
 void ledToggleTask(void * pargs){
 	while(1){
 	int led=*(int*)pargs;
